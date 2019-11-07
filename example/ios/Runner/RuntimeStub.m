0df991119c8522aa86d4c362626b958d0253ba9c
yulingtianxia <yulingtianxia@gmail.com>
2019-11-07 21:36:51 +0800

    Update RuntimeStub.m

--- example/ios/Runner/RuntimeStub.m
+++ example/ios/Runner/RuntimeStub.m
@@ -34,124 +34,120 @@
 
 - (BOOL)fooBOOL:(BOOL)a
 {
-    NSLog(@"%d", a);
+    NSLog(@"%s arg: %d", __FUNCTION__, a);
     return a;
 }
 
 - (int8_t)fooInt8:(int8_t)a
 {
-    NSLog(@"arg: %d", a);
+    NSLog(@"%s arg: %d", __FUNCTION__, a);
     return -123;
 }
 
 - (int16_t)fooInt16:(int16_t)a
 {
-    NSLog(@"arg: %d", a);
+    NSLog(@"%s arg: %d", __FUNCTION__, a);
     return -12345;
 }
 
 - (int32_t)fooInt32:(int32_t)a
 {
-    NSLog(@"arg: %d", a);
+    NSLog(@"%s arg: %d", __FUNCTION__, a);
     return -123456;
 }
 
 - (int64_t)fooInt64:(int64_t)a
 {
-    NSLog(@"arg: %lld", a);
+    NSLog(@"%s arg: %lld", __FUNCTION__, a);
     return -123456;
 }
 
 - (uint8_t)fooUInt8:(uint8_t)a
 {
-    NSLog(@"arg: %d", a);
+    NSLog(@"%s arg: %d", __FUNCTION__, a);
     return 123;
 }
 
 - (uint16_t)fooUInt16:(uint16_t)a
 {
-    NSLog(@"arg: %d", a);
+    NSLog(@"%s arg: %d", __FUNCTION__, a);
     return 12345;
 }
 
 - (uint32_t)fooUInt32:(uint32_t)a
 {
-    NSLog(@"arg: %d", a);
+    NSLog(@"%s arg: %d", __FUNCTION__, a);
     return 123456;
 }
 
 - (uint64_t)fooUInt64:(uint64_t)a
 {
-    NSLog(@"arg: %llu", a);
+    NSLog(@"%s arg: %llu", __FUNCTION__, a);
     return 123456;
 }
 
 - (float)fooFloat:(float)a
 {
-    NSLog(@"arg: %f", a);
+    NSLog(@"%s arg: %f", __FUNCTION__, a);
     return 123.456;
 }
 
 - (double)fooDouble:(double)a
 {
-    NSLog(@"arg: %f", a);
+    NSLog(@"%s arg: %f", __FUNCTION__, a);
     return 123.456;
 }
 
 - (char)fooChar:(char)a
 {
-    NSLog(@"arg: %c", a);
+    NSLog(@"%s arg: %c", __FUNCTION__, a);
     return a;
 }
 
 - (unsigned char)fooUChar:(unsigned char)a
 {
-    NSLog(@"arg: %c", a);
+    NSLog(@"%s arg: %c", __FUNCTION__, a);
     return a;
 }
 
 - (char *)fooCharPtr:(char *)a
 {
-    NSLog(@"arg: %s", a);
+    NSLog(@"%s arg: %s", __FUNCTION__, a);
     return a;
 }
 
 - (Class)fooClass:(Class)a
 {
-    NSLog(@"arg: %@", a);
+    NSLog(@"%s arg: %@", __FUNCTION__, a);
     return [RuntimeStub class];
 }
 
 - (SEL)fooSEL:(SEL)a
 {
-    NSLog(@"arg: %@", NSStringFromSelector(a));
+    NSLog(@"%s arg: %@", __FUNCTION__, NSStringFromSelector(a));
     return _cmd;
 }
 
 - (id)fooObject:(id)a
 {
-    NSLog(@"arg: %@", a);
+    NSLog(@"%s arg: %@", __FUNCTION__, a);
     return self.object;
 }
 
 - (void *)fooPointer:(void *)a
 {
-    NSLog(@"arg: %@", a);
+    NSLog(@"%s arg: %@", __FUNCTION__, a);
     return (__bridge void *)(self);
 }
 
 - (void)fooVoid
 {
-    NSLog(@"foo15 called");
-    return;
+    NSLog(@"%s called", __FUNCTION__);
 }
 
 - (CGRect)fooCGRect:(CGRect)rect
 {
-    NSLog(@"%s %f, %f, %f, %f", __func__, rect.origin.x, rect.origin.y, rect.size.width, rect.size.height);
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        NSLog(@"%s %f, %f, %f, %f", __func__, rect.origin.x, rect.origin.y, rect.size.width, rect.size.height);
-    });
+    NSLog(@"%s %f, %f, %f, %f", __FUNCTION__, rect.origin.x, rect.origin.y, rect.size.width, rect.size.height);
     return (CGRect){1, 2, 3, 4};
 }
 
@@ -162,7 +158,7 @@ typedef int(^BarBlock)(NSObject *a);
     NSObject *arg = [NSObject new];
     dispatch_async(dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0), ^{
         int result = block(arg);
-        NSLog(@"---result: %d", result);
+        NSLog(@"%s result: %d", __FUNCTION__, result);
     });
     
     BarBlock bar = ^(NSObject *a) {
@@ -175,14 +171,16 @@ typedef int(^BarBlock)(NSObject *a);
 
 - (void)fooDelegate:(id<StubDelegate>)delegate
 {
+    NSLog(@"%s arg: %@", __FUNCTION__, delegate);
     dispatch_async(dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0), ^{
         [delegate callback];
+        NSLog(@"%s callback", __FUNCTION__);
     });
 }
 
 - (NSString *)fooNSString:(NSString *)str
 {
-    NSLog(@"%@", str);
+    NSLog(@"%s arg: %@", __FUNCTION__, str);
     return @"test nsstring";
 }
 
