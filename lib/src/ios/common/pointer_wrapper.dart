08e968c7672d0c10f7f7927b0be0584d72ec70a8
杨萧玉 <yulingtianxia@gmail.com>
2020-02-23 15:01:45 +0800

    support struct dealloc callback

--- lib/src/ios/common/pointer_wrapper.dart
+++ lib/src/ios/common/pointer_wrapper.dart
@@ -5,10 +5,20 @@ import 'package:dart_native/src/ios/runtime/nsobject.dart';
 import 'package:dart_native/src/ios/runtime/selector.dart';
 
 class PointerWrapper extends NSObject {
-  PointerWrapper() : super(Class('DNPointerWrapper'));
+  PointerWrapper([this._pointerDealloc]) : super(Class('DNPointerWrapper'));
+  // Using for calling pointer's Dart class dealloc.
+  Function _pointerDealloc;
 
   Pointer<Void> get value => perform(Selector('pointer'));
   set value(Pointer<Void> ptr) {
     perform(Selector('setPointer:'), args: [ptr]);
+  }
+
+  @override
+  dealloc() {
+    if (_pointerDealloc != null) {
+      _pointerDealloc();
+    }
+    return super.dealloc();
   }
 }
