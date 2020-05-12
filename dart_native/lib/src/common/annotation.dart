114f9b79ddf5890d93db71cc2713d5aa62b55f48
杨萧玉 <yulingtianxia@gmail.com>
2020-05-12 17:14:59 +0800

    feat: https://github.com/dart-native/dart_native/issues/7

--- dart_native/lib/src/common/annotation.dart
+++ dart_native/lib/src/common/annotation.dart
@@ -1,3 +1,26 @@
-class Available {
-  const Available(String version0, [String version1, String version2, String version3, String version4]);
+class NativeAvailable {
+  final String ios;
+  final String macos;
+  final String tvos;
+  final String watchos;
+  final String android;
+  const NativeAvailable({this.ios, this.macos, this.tvos, this.watchos, this.android});
+}
+
+class NativeUnavailable {
+  final String ios;
+  final String macos;
+  final String tvos;
+  final String watchos;
+  final String android;
+  const NativeUnavailable({this.ios, this.macos, this.tvos, this.watchos, this.android});
+}
+
+class NativeDeprecated {
+  final List<String> ios;
+  final List<String> macos;
+  final List<String> tvos;
+  final List<String> watchos;
+  final List<String> android;
+  const NativeDeprecated(String message, {this.ios, this.macos, this.tvos, this.watchos, this.android});
 }
