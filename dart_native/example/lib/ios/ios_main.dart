3af52f7d3cfa0d93fd9fc04a10a05d4a2e0d5398
杨萧玉 <yulingtianxia@gmail.com>
2020-09-26 23:21:40 +0800

    feat: add benchmark

--- dart_native/example/lib/ios/ios_main.dart
+++ dart_native/example/lib/ios/ios_main.dart
@@ -4,12 +4,16 @@ import 'package:dart_native_example/ios/unit_test.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
+import 'package:flutter/services.dart';
+
 class IOSApp extends StatefulWidget {
   @override
   _IOSAppState createState() => _IOSAppState();
 }
 
 class _IOSAppState extends State<IOSApp> {
+  static const platform = const MethodChannel('sample.dartnative.com');
+
   @override
   void initState() {
     super.initState();
@@ -21,6 +25,25 @@ class _IOSAppState extends State<IOSApp> {
     RuntimeSon stub = RuntimeSon();
     DelegateStub delegate = DelegateStub();
     testIOS(stub, delegate);
+
+    // Benchmark
+    String testString =
+        'This is a long string: sdlfdksjflksndhiofuu2893873(*（%￥#@）*&……￥撒肥料开发时傅雷家书那份会计师东方丽景三等奖';
+    int time = DateTime.now().millisecondsSinceEpoch;
+
+    for (var i = 0; i < 10000; i++) {
+      String _ = await platform.invokeMethod('fooNSString:', testString);
+    }
+
+    print(
+        "Flutter Channel Cost: ${DateTime.now().millisecondsSinceEpoch - time}");
+    time = DateTime.now().millisecondsSinceEpoch;
+
+    for (var i = 0; i < 10000; i++) {
+      String _ = stub.fooNSString(testString);
+    }
+
+    print("DartNative Cost: ${DateTime.now().millisecondsSinceEpoch - time}");
   }
 
   @override
