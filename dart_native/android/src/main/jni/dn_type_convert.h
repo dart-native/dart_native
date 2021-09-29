befa3e75b58f8d74f6a6754430a479c060a37f1c
hui19 <zhouh1206@163.com>
2021-09-29 19:02:51 +0800

    refactor: refactor type convert map

--- dart_native/android/src/main/jni/dn_type_convert.h
+++ dart_native/android/src/main/jni/dn_type_convert.h
@@ -13,30 +13,13 @@ extern "C"
 {
 #endif
 
-  typedef void BasicTypeToNative(void *, jvalue *, int);
+typedef void BasicTypeToNative(void *, jvalue *, int);
 
-  jstring convertToJavaUtf16(JNIEnv *env, void *value);
-  uint16_t *convertToDartUtf16(JNIEnv *env, jstring nativeString);
+jstring convertToJavaUtf16(JNIEnv *env, void *value);
 
-  void convertToJChar(void *value, jvalue *argValue, int index);
-  void convertToJInt(void *value, jvalue *argValue, int index);
-  void convertToJDouble(void *value, jvalue *argValue, int index);
-  void convertToJFloat(void *value, jvalue *argValue, int index);
-  void convertToJByte(void *value, jvalue *argValue, int index);
-  void convertToJShort(void *value, jvalue *argValue, int index);
-  void convertToJLong(void *value, jvalue *argValue, int index);
-  void convertToJBoolean(void *value, jvalue *argValue, int index);
+uint16_t *convertToDartUtf16(JNIEnv *env, jstring nativeString);
 
-  /// key is basic argument signature
-  const std::map<char, std::function<BasicTypeToNative> > basicTypeConvertMap = {
-      {'C', convertToJChar},
-      {'I', convertToJInt},
-      {'D', convertToJDouble},
-      {'F', convertToJFloat},
-      {'B', convertToJByte},
-      {'S', convertToJShort},
-      {'J', convertToJLong},
-      {'Z', convertToJBoolean}};
+std::map<char, std::function<BasicTypeToNative>> GetTypeConvertMap();
 
 #ifdef __cplusplus
 }
